8aff7d72d908c2ab7342cb4562a5c3ed9d9dbfed
Mayank Kumar <mayank.k9802@gmail.com>
2023-06-06 16:29:18 +0530

    Header Updated

--- ware/src/Pages/Homepage/Components/Header/Header.jsx
+++ ware/src/Pages/Homepage/Components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Header.css'
 import HeaderUp from './HeaderUp'
+import HeaderDown from './HeaderDown'
 
 export default function Header() {
   return (
     <div className='head-main'>
         <HeaderUp/>
         <span>WareHouse Fed</span>
+        <HeaderDown/>
     </div>
   )
 }
---
+++ ware/src/Pages/Homepage/Components/Header/HeaderDown.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import Fb from "./Assets/fb.svg";
+import In from "./Assets/in.svg";
+import Tw from "./Assets/tw.svg";
+import Yt from "./Assets/yt.svg";
+import Notification from "./Assets/notification.svg";
+import Search from "./Assets/search.svg";
+
+export default function HeaderDown() {
+  return (
+    <div className="head-down">
+      <div className="search-ar">
+        <input
+          className="search-box"
+          type="text"
+          placeholder="Search here..."
+        />
+        <button className="srbtn">
+          <img src={Search} alt="" />
+        </button>
+      </div>
+
+      <div className="hd-links">
+        <ul>
+          <li>
+            <Link to="">About HSWC</Link>
+            <span className="hdspan" />
+          </li>
+          <li>
+            <Link to="">Photo Gallery</Link>
+            <span className="hdspan" />
+          </li>
+          <li>
+            <Link to="">File RTI</Link>
+            <span className="hdspan" />
+          </li>
+          <li>
+            <Link to="">Contact Us</Link>
+            <span className="hdspan" />
+          </li>
+          <li>
+            <Link to="">
+              <img src={Fb} alt="facebook" />
+            </Link>
+            <Link to="">
+              <img src={In} alt="instagram" />
+            </Link>
+            <Link to="">
+              <img src={Tw} alt="twitter" />
+            </Link>
+            <Link to="">
+              <img src={Yt} alt="youtube" />
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="hd-notification">
+        Notifications
+        <img src={Notification} alt="notification" />
+      </div>
+    </div>
+  );
+}
