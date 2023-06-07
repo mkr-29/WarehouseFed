b11d118edfcd1a79edfe178ab686edb5ba3467a9
Mayank Kumar <mayank.k9802@gmail.com>
2023-06-07 21:47:56 +0530

    Landing Left Done

--- ware/src/Pages/Homepage/Components/Landing/Carousel1.jsx
+++ ware/src/Pages/Homepage/Components/Landing/Carousel1.jsx
@@ -1,11 +1,10 @@
-import React, { useRef } from "react";
+import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import "./Carousel1.css"
 
 export default function Carousel1(parse) {
-  const carouselRef = useRef(null);
   const settings = {
     dots: false,
     infinite: true,
@@ -14,51 +13,21 @@ export default function Carousel1(parse) {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-  };
-  const goToNextSlide = () => {
-    carouselRef.current.slickNext();
-  };
-
-  const goToPrevSlide = () => {
-    carouselRef.current.slickPrev();
   };
   return (
     <div>
-      <Slider {...settings} ref={carouselRef}>
-        <div>
+      <Slider {...settings}>
+        <div className="textcar">
           <h2>{parse.heading1}</h2>
           <h3>{parse.subheading1}</h3>
           <p>{parse.about1}</p>
         </div>
-        <div>
+        <div className="textcar">
           <h2>{parse.heading2}</h2>
           <h3>{parse.subheading2}</h3>
           <p>{parse.about2}</p>
         </div>
       </Slider>
-      <div>
-        <button onClick={goToPrevSlide}>Previous</button>
-        <button onClick={goToNextSlide}>Next</button>
-      </div>
-    </div>
-  );
-}
-function NextArrow(props) {
-  const { className, onClick } = props;
-  return (
-    <div className={className} onClick={onClick}>
-      Next
-    </div>
-  );
-}
-
-function PrevArrow(props) {
-  const { className, onClick } = props;
-  return (
-    <div className={className} onClick={onClick}>
-      Previous
     </div>
   );
 }
