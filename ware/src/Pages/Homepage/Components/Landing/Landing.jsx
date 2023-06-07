ca68c897365b973891eedecfb04c0c674979690c
Mayank Kumar <mayank.k9802@gmail.com>
2023-06-07 22:07:52 +0530

    Landing Done

--- ware/src/Pages/Homepage/Components/Landing/Landing.jsx
+++ ware/src/Pages/Homepage/Components/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Landing.css"
 import Carousel1 from './Carousel1'
+import {Link} from "react-router-dom"
 
 export default function Landing() {
   return (
@@ -18,6 +19,17 @@ export default function Landing() {
                 advanced technologies and strategic expertise to unlock the full potential 
                 of your storage capabilities, empowering your business for growth and success."
             />
+        </div>
+        <div className="landing-r">
+            <h3>Quick Links</h3>
+            <div className="landing-rql">
+                <Link to=''>E-Governance Login</Link>
+                <Link to=''>Employee Corner</Link>
+                <Link to=''>Farmer Registration</Link>
+                <Link to=''>Scientific Storage</Link>
+                <Link to=''>Farmer Registration</Link>
+                <Link to=''>Scientific Storage</Link>
+            </div>
         </div>
     </div>
   )
