22d761dfd25ae10e878d95c8ce7a6f6d05503f1c
Mayank Kumar <mayank.k9802@gmail.com>
2023-06-14 16:38:12 +0530

    C1

--- ware/src/Pages/Homepage/Components/Header/HeaderUp.jsx
+++ ware/src/Pages/Homepage/Components/Header/HeaderUp.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import Logo1 from "./Assets/logo1.png";
 import Logo2 from "./Assets/logo2.png";
 import { Link } from "react-router-dom";
 import Drop from "./Assets/drop.svg";
+import Hamburger from 'hamburger-react'
 
 export default function HeaderUp() {
+  const [isOpen, setOpen] = useState(false)
+
+  use hamburger menue for devices with width less than 1000px
+
   return (
     <div id="header">
       <div className="h-logos">
         <img src={Logo1} alt="logo1" />
         <img src={Logo2} alt="logo2" />
       </div>
-      <div className="h-links">
+
+      <Hamburger toggled={isOpen} toggle={setOpen} />
+      
+      <div className={`h-links ${isOpen ? 'open' : ''}`}>
         <ul>
           {/* Home */}
           <li>
@@ -35,138 +43,8 @@ export default function HeaderUp() {
               <br />
               heritage and pride
             </p>
-            <div class="dropdown-menu">
-              <ul>
-                <li>
-                  <Link to="">About</Link>
-                  <div class="dropdown-menu-1">
-                    <ul>
-                      <li>
-                        <a href="#">News and Events</a>
-                      </li>
-                      <li>
-                        <a href="">FAQ's</a>
-                      </li>
-                      <li>
-                        <a href="#">Manufacturing</a>
-                      </li>
-                    </ul>
-                  </div>
-                </li>
-                <li>
-                  <a href="#">
-                    Product Categorization <i class="fas fa-caret-right"></i>
-                  </a>
-                  <div class="dropdown-menu-1">
-                    <ul>
-                      <li>
-                        <a href="./Pages/ProductCategorisation/Absorption.html">
-                          Absorption
-                        </a>
-                      </li>
-                      <li>
-                        <a href="./Pages/ProductCategorisation/Insulation.html">
-                          Insulation
-                        </a>
-                      </li>
-                      <li>
-                        <a href="./Pages/ProductCategorisation/Vibration.html">
-                          Vibration
-                        </a>
-                      </li>
-                      <li>
-                        <a href="./Pages/ProductCategorisation/Diffusion.html">
-                          Diffusion
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </li>
-                <li>
-                  <a href="#">
-                    Product Range <i class="fas fa-caret-right"></i>
-                  </a>
-                  <div class="dropdown-menu-1">
-                    <ul>
-                      <li>
-                        <a href="./Pages/ProductRange/Ceiling.html">Ceiling</a>
-                      </li>
-                      <li>
-                        <a href="./Pages/ProductRange/Walls.html">Walls</a>
-                      </li>
-                      <li>
-                        <a href="./Pages/ProductRange/Floor.html">Floor</a>
-                      </li>
-                      <li>
-                        <a href="./Pages/ProductRange/Door.html">Door</a>
-                      </li>
-                      <li>
-                        <a href="./Pages/ProductRange/Windows.html">Windows</a>
-                      </li>
-                      <li>
-                        <a href="./Pages/ProductRange/MovablePartitions.html">
-                          Movable Partitions
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </li>
-                <li>
-                  <a href="#">
-                    Applications <i class="fas fa-caret-right"></i>
-                  </a>
-                  <div class="dropdown-menu-1">
-                    <ul>
-                      <li>
-                        <a href="./Pages/Applications/Auditoriums.html">
-                          Auditorium
-                        </a>
-                      </li>
-                      <li>
-                        <a href="./Pages/Applications/Banquet.html">
-                          Banquel Halls
-                        </a>
-                      </li>
-                      <li>
-                        <a href="./Pages/Applications/Conference.html">
-                          Conference Halls
-                        </a>
-                      </li>
-                      <li>
-                        <a href="./Pages/Applications/Disco.html">
-                          Discos and Launges
-                        </a>
-                      </li>
-                      <li>
-                        <a href="./Pages/Applications/Gymnasiums.html">
-                          Gymnasiums
-                        </a>
-                      </li>
-                      <li>
-                        <a href="./Pages/Applications/HomeTheater.html">
-                          Home Theaters
-                        </a>
-                      </li>
-                      <li>
-                        <a href="./Pages/Applications/Multipurpose.html">
-                          Multi-Purpose Halls
-                        </a>
-                      </li>
-                      <li>
-                        <a href="./Pages/Applications/Office.html">Office</a>
-                      </li>
-                      <li>
-                        <a href="./Pages/Applications/Studios.html">Studios</a>
-                      </li>
-                      <li>
-                        <a href="./Pages/Applications/Worship.html">
-                          Worship Places
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </li>
-              </ul>
+            <div className="dropdown-menu">
+              {/* Dropdown menu content */}
             </div>
           </li>
 
@@ -181,15 +59,8 @@ export default function HeaderUp() {
               <br />
               you
             </p>
-            <div class="dropdown-menu">
-              <ul>
-                <li>
-                  <Link to="">About</Link>
-                </li>
-                <li>
-                  <a href="#">Board of Directors</a>
-                </li>
-              </ul>
+            <div className="dropdown-menu">
+              {/* Dropdown menu content */}
             </div>
           </li>
 
@@ -204,15 +75,8 @@ export default function HeaderUp() {
               <br />
               information
             </p>
-            <div class="dropdown-menu">
-              <ul>
-                <li>
-                  <Link to="">About</Link>
-                </li>
-                <li>
-                  <a href="#">Board of Directors</a>
-                </li>
-              </ul>
+            <div className="dropdown-menu">
+              {/* Dropdown menu content */}
             </div>
           </li>
 
@@ -227,15 +91,8 @@ export default function HeaderUp() {
               <br />
               business opportunities
             </p>
-            <div class="dropdown-menu">
-              <ul>
-                <li>
-                  <Link to="">About</Link>
-                </li>
-                <li>
-                  <a href="#">Board of Directors</a>
-                </li>
-              </ul>
+            <div className="dropdown-menu">
+              {/* Dropdown menu content */}
             </div>
           </li>
 
@@ -250,15 +107,8 @@ export default function HeaderUp() {
               <br />
               business opportunities
             </p>
-            <div class="dropdown-menu">
-              <ul>
-                <li>
-                  <Link to="">About</Link>
-                </li>
-                <li>
-                  <a href="#">Board of Directors</a>
-                </li>
-              </ul>
+            <div className="dropdown-menu">
+              {/* Dropdown menu content */}
             </div>
           </li>
         </ul>
